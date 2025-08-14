7bae4d988334fff686465dc39ae6dc58ba79ce79
Nikolai Reed-Larsen <nikolai.reedlarsen@fjordline.com>
2025-08-14 10:39:28 +0200

    feat: enhance migration script to include language support and translations for package rules

--- scripts/migration/migrate-packageRules.ts
+++ scripts/migration/migrate-packageRules.ts
@@ -2,6 +2,7 @@ import { sanityFetch } from "@/lib/sanity/live";
 import { sanityApiClient } from "@/lib/sanity/client";
 import { Bundle_v2 } from "@fjordline/sanity-types";
 import { v4 as uuidv4 } from "uuid";
+import { Locale } from "@/types/locales";
 
 const client = sanityApiClient;
 
@@ -11,16 +12,30 @@ async function migrateBundleRules() {
     _id,
     _rev,
     name,
-    bundlePackageRules
+    language,
+    bundlePackageRules,
+    "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
+      name,
+      "slug": slug.current,
+      language,
+      _id
+    }
   }`,
   };
   const res = await sanityFetch(sanityFetchConfig);
-  const bundles: Bundle_v2[] = res.data;
+  const bundles: (Bundle_v2 & {
+    _translations: {
+      slug: string | null;
+      name: string | null;
+      language: Locale;
+      _id: string;
+    }[];
+  })[] = res.data;
 
   console.log(`Found ${bundles.length} bundles to migrate`);
 
   for (const bundle of bundles) {
-    const { _id, name, bundlePackageRules } = bundle;
+    const { _id, name, bundlePackageRules, _translations } = bundle;
 
     const ruleDocId = uuidv4();
 
@@ -28,6 +43,7 @@ async function migrateBundleRules() {
     await client.createIfNotExists({
       _id: ruleDocId,
       _type: "packageRule_v2",
+      availableLanguages: _translations.map((t) => t.language),
       name: name ?? `Rule for ${_id}`,
       description: `Migrated from bundle ${_id}`,
       rules: bundlePackageRules,
